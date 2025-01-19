3d6ee672c12ebcc0ef94e296ea9bb7f164e1d84b
SonaniAkshit <sonaniakshit777@gmail.com>
2025-01-20 00:42:13 +0530

    Update Invoice_Client.java

--- Factory-Design-Pattern/src/Invoice/Invoice_Client.java
+++ Factory-Design-Pattern/src/Invoice/Invoice_Client.java
@@ -6,12 +6,24 @@ public class Invoice_Client {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Invoice invoice;
-        int choice;
-        choice = sc.nextInt();
+        while (true) {
+            Invoice invoice;
+            int choice;
+            System.out.println("\n****menu****\n1)Invoice with all\n2)Invoice with header\n3)Invoice with footer\n4)Invoice without all\n5)Invoice without footer with header\n6)Invoice without header with footer\n0 for Exit\n************\n");
+            choice = sc.nextInt();
 
-        invoice = InvoiceFactory.getInvoice(choice);
+            if (choice == 0) {
+                System.out.println("Exiting...");
+                break;
+            }
 
-        System.out.println(invoice.getItype());
+            invoice = InvoiceFactory.getInvoice(choice);
+
+            if (invoice != null) {
+                System.out.println(invoice.getItype());
+            } else {
+                System.out.println("Invalid choice.");
+            }
+        }
     }
 }
