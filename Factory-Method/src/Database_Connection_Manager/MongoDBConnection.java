32d4384d5d46a0f9b437ac22a1f81255b6c3fdf9
SonaniAkshit <sonaniakshit777@gmail.com>
2025-01-20 02:15:30 +0530

    commit

--- Factory-Method/src/Database_Connection_Manager/MongoDBConnection.java
+++ Factory-Method/src/Database_Connection_Manager/MongoDBConnection.java
@@ -1,4 +1,23 @@
 package Database_Connection_Manager;
 
-public class MongoDBConnection {
+public class MongoDBConnection implements DatabaseConnection {
+
+    String DBconnection;
+    String DBdisconnection;
+    String DBquery;
+    MongoDBConnection(){
+        DBconnection = "Connected to MongoDB Database.";
+        DBdisconnection = "Disconnected from MongoDB Database.";
+        DBquery="Executing query: db.collection.find({});";
+    }
+
+    public String connect() {
+        return DBconnection;
+    }
+    public String disconnect() {
+        return DBdisconnection;
+    }
+    public String query() {
+        return DBquery;
+    }
 }
