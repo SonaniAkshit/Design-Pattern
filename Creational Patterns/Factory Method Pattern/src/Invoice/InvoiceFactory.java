9b11a18ed49415900d63df051560612d04f93f11
SonaniAkshit <sonaniakshit684@gmail.com>
2025-04-09 23:16:22 +0530

    commit

--- Creational Patterns/Factory Method Pattern/src/Invoice/InvoiceFactory.java
+++ Creational Patterns/Factory Method Pattern/src/Invoice/InvoiceFactory.java
@@ -2,28 +2,7 @@ package Invoice;
 
 public class InvoiceFactory {
     public static Invoice getInvoice(int choice) {
-        Invoice I = null;
-        switch (choice) {
-            case 1:
-                I = new Invoice_with_all();
-                break;
-            case 2:
-                I = new Invoice_with_header();
-                break;
-            case 3:
-                I = new Invoice_with_footer();
-                break;
-            case 4:
-                I = new Invoice_without_all();
-                break;
-            case 5:
-                I = new Invoice_without_header_with_footer();
-                break;
-            case 6:
-                I = new Invoice_without_footer_with_header();
-                break;
-        }
-        return I;
+        return null;
     }
 }
 
